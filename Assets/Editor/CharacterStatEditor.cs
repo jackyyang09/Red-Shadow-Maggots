79bf5f94640a0231421fea1b2350841c85e8cae1
Brogrammist <jackyyang267@gmail.com>
2023-05-16 22:14:51 -0400

    Updated CharacterStatEditor

--- Assets/Editor/CharacterStatEditor.cs
+++ Assets/Editor/CharacterStatEditor.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using JackysEditorHelpers;
 
 [System.Serializable]
 public class ClassStats
@@ -17,10 +18,16 @@ public class CharacterStatEditor : ScriptableSingleton<CharacterStatEditor>
     public Vector2 StatPool = new Vector2(2000, 20000);
     public float EnemyHealthMultiplier = 3;
     public float BaseDeviancy = 0.05f;
+    public ClassStats[] ClassStats;
+
+    public string CharacterPath;
+    public CharacterObject[] CharacterReferences;
 }
 
 public class CharacterStatWindow : EditorWindow
 {
+    public CharacterStatEditor Instance => CharacterStatEditor.instance;
+
     static CharacterStatWindow Window;
 
     [MenuItem(RSMEditorTools.RSM_TOOLS_MENU + "Character Stat Window")]
@@ -29,8 +36,95 @@ public class CharacterStatWindow : EditorWindow
         Window = GetWindow<CharacterStatWindow>();
     }
 
+    private void OnEnable() 
+    {
+        if (Instance.ClassStats == null)
+        {
+            Instance.ClassStats = new ClassStats[3];
+            Instance.ClassStats[(int)CharacterClass.Offense] = new ClassStats();
+            Instance.ClassStats[(int)CharacterClass.Offense].StatDeviancy = 0.25f;
+            Instance.ClassStats[(int)CharacterClass.Offense].CritRate = 0.01f;
+            Instance.ClassStats[(int)CharacterClass.Offense].CritDamage = 0.5f;
+
+            Instance.ClassStats[(int)CharacterClass.Defense] = new ClassStats();
+            Instance.ClassStats[(int)CharacterClass.Defense].StatDeviancy = 0.15f;
+            Instance.ClassStats[(int)CharacterClass.Defense].CritRate = 0.025f;
+            Instance.ClassStats[(int)CharacterClass.Defense].CritDamage = 0.5f;
+
+            Instance.ClassStats[(int)CharacterClass.Support] = new ClassStats();
+            Instance.ClassStats[(int)CharacterClass.Support].StatDeviancy = 0.175f;
+            Instance.ClassStats[(int)CharacterClass.Support].CritRate = 0.05f;
+            Instance.ClassStats[(int)CharacterClass.Support].CritDamage = 0.5f;
+        }
+    }
+
     private void OnGUI()
     {
-        EditorGUILayout.FloatField(CharacterStatEditor.instance.EnemyHealthMultiplier);
+        Undo.RecordObject(Instance, "Changed Character Stat Editor Props");
+
+        EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+        Instance.CharacterPath = EditorGUILayout.TextField("Character Object Path", Instance.CharacterPath);
+        if (Instance.CharacterPath == null) EditorGUILayout.LabelField("Loaded Characters", "0");
+        EditorGUILayout.LabelField("Loaded Characters", Instance.CharacterReferences.Length.ToString());
+        if (GUILayout.Button("Load Characters At Path"))
+        {
+            Instance.CharacterReferences = EditorHelper.ImportAssetsOrFoldersAtPath<CharacterObject>(Instance.CharacterPath).ToArray();
+        }
+        EditorGUILayout.EndVertical();
+
+        Instance.EnemyHealthMultiplier = EditorGUILayout.FloatField("Enemy Health Multiplier", Instance.EnemyHealthMultiplier);
+        Instance.BaseDeviancy = EditorGUILayout.FloatField("Base Deviancy", Instance.BaseDeviancy);
+        Instance.StatPool = EditorGUILayout.Vector2Field("Stat Pool Range", Instance.StatPool);
+
+        for (int i = 0; i < Instance.ClassStats.Length; i++)
+        {
+            EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+            EditorGUILayout.LabelField(((CharacterClass)i).ToString(), TF2Ls.TF2LsStyles.CenteredLabel);
+            Instance.ClassStats[i].StatDeviancy = EditorGUILayout.Slider("Stat Deviancy", Instance.ClassStats[i].StatDeviancy, 0, 1);
+            Instance.ClassStats[i].CritRate = EditorGUILayout.FloatField("Crit Rate", Instance.ClassStats[i].CritRate);
+            Instance.ClassStats[i].CritDamage = EditorGUILayout.FloatField("Crit Damage", Instance.ClassStats[i].CritDamage);
+            EditorGUILayout.EndVertical();
+        }
+
+        if (GUILayout.Button("Apply New Stats to Characters"))
+        {
+            RandomizeStatsForAllCharacters();
+        }
+    }
+
+    float RandomDeviancy => Random.Range(-Instance.BaseDeviancy, Instance.BaseDeviancy);
+
+    void RandomizeStatsForAllCharacters()
+    {
+        foreach (var character in Instance.CharacterReferences)
+        {
+            Vector2 attackRange;
+            Vector2 healthRange;
+            Vector2 enemyHealthRange;
+            float attackDeviancy = 0;
+            var characterClass = (int)character.characterClass;
+
+            var stats = Instance.ClassStats[characterClass];
+
+            attackDeviancy = stats.StatDeviancy + RandomDeviancy;
+
+            float healthDeviancy = 1 - attackDeviancy;
+
+            attackRange.x = (int)Mathf.LerpUnclamped(0, Instance.StatPool.x, attackDeviancy);
+            healthRange.x = (int)Mathf.LerpUnclamped(0, Instance.StatPool.x, healthDeviancy);
+            attackRange.y = (int)Mathf.LerpUnclamped(0, Instance.StatPool.y, attackDeviancy);
+            healthRange.y = (int)Mathf.LerpUnclamped(0, Instance.StatPool.y, healthDeviancy);
+
+            enemyHealthRange.x = healthRange.x * Instance.EnemyHealthMultiplier;
+            enemyHealthRange.y = healthRange.y * Instance.EnemyHealthMultiplier;
+
+            SerializedObject so = new SerializedObject(character);
+            so.FindProperty(nameof(attackRange)).vector2Value = attackRange;
+            so.FindProperty(nameof(healthRange)).vector2Value = healthRange;
+            so.FindProperty(nameof(enemyHealthRange)).vector2Value = enemyHealthRange;
+            so.FindProperty("critChance").floatValue = Instance.ClassStats[characterClass].CritRate;
+            so.FindProperty("critDamageMultiplier").floatValue = Instance.ClassStats[characterClass].CritDamage;
+            so.ApplyModifiedProperties();
+        }
     }
 }
