3648205fd4b336230be4620caa79bead39bd3f8c
Brogrammist <jackyyang267@gmail.com>
2023-10-15 02:01:05 -0400

    Fixed TotalDamageCounter not counting total damage

--- Assets/Scripts/BattleScene/UI/TotalDamageCounter.cs
+++ Assets/Scripts/BattleScene/UI/TotalDamageCounter.cs
@@ -7,7 +7,7 @@ public class TotalDamageCounter : BasicSingleton<TotalDamageCounter>
     [SerializeField] OptimizedCanvas canvas;
     [SerializeField] TMPro.TextMeshProUGUI damageLabel;
 
-    float damage;
+    float damageTotal = 0;
 
     void ShowUI() => canvas.Show();
     void HideUI() => canvas.Hide();
@@ -28,13 +28,12 @@ public class TotalDamageCounter : BasicSingleton<TotalDamageCounter>
 
     void AddDamage(BaseCharacter character, DamageStruct damage)
     {
-        float damageCount = 0;
         if (!canvas.IsVisible)
         {
-            damageCount = 0;
+            damageTotal = 0;
             canvas.Show();
         }
-        damageCount += damage.TotalDamage;
-        damageLabel.text = ((int)damageCount).ToString();
+        damageTotal += damage.TotalDamage;
+        damageLabel.text = ((int)damageTotal).ToString();
     }
 }
